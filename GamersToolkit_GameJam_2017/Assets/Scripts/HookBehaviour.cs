4ef6989a4c9eb59c4772f728ee9c352b1f67b4cf
joeyGumer <pep3553@gmail.com>
2017-07-14 22:01:23 +0200

    Testing Hook Gameplay

---
+++ GamersToolkit_GameJam_2017/Assets/Scripts/HookBehaviour.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookBehaviour : MonoBehaviour {
+
+    public Vector3 target;
+    public float max_distance = 10;
+    public float speed;
+    public bool to_destroy = false;
+
+    private Vector3 direction;
+    private Vector3 player_position;
+    private float distance;
+
+    //[---read only----!]
+    public bool grabed;
+
+    delegate void HookState();
+    HookState hookState;
+
+
+    // Use this for initialization
+    void Start () {
+
+        grabed = false;
+        player_position = transform.position;
+        direction = (target - transform.position).normalized;
+        hookState = Throw;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        hookState();
+	}
+
+    //Utils
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "grab")
+            grabed = true;
+            
+    }
+
+    void Move()
+    {
+        Vector3 mov = direction * speed * Time.deltaTime;
+        transform.Translate(mov.x, mov.y, 0);
+
+        distance = (transform.position - player_position).magnitude;
+    }
+
+    //Hook behaviour states
+    void Throw()
+    {
+        Move();
+
+        if (distance >= max_distance)
+        {
+            hookState = Return;
+            direction = -direction;
+        }
+        else if(grabed)
+        {
+            hookState = Go;
+        }
+
+        
+    }
+    
+    void Go()
+    {
+
+    }
+
+    void Return()
+    {
+        Move();
+
+        if(distance <= 0.2)
+        {
+            Destroy(this);
+        }
+    }
+
+}
---
+++ GamersToolkit_GameJam_2017/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    //Public attributes
+    public float speed = 10.0f;
+    public GameObject hookPrefab;
+
+    //Private attributes
+    private bool throwingHook = false;
+    private GameObject hook = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!throwingHook)
+            BasicMovement();
+
+        if(Input.GetKeyDown("space"))
+            ThrowHook();
+    }
+
+
+    //Methods
+    //Basic movement for testint
+    void BasicMovement()
+    {
+            
+        float movement = (Input.GetAxis("Horizontal") * speed) * Time.deltaTime;
+
+        transform.Translate(movement,0, 0);
+    }
+
+    //Throws the Hook
+    void ThrowHook()
+    {
+        throwingHook = true;
+
+       hook = Instantiate(hookPrefab, transform);
+
+        hook.GetComponent<HookBehaviour>().target = Input.mousePosition;
+    }
+
+}
